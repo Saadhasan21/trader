194c1bdd28fd90b8709392253f51742e04f87ca6
Saad <saad.h@valuebound.com>
2021-10-03 20:35:21 +0530

    learnt and tried

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Viewalgo from "./components/Viewalgo";
 import Bots from "./components/Bots";
@@ -6,47 +6,50 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 
 function App() {
 
 
- let bots=[
+  let data = [
     {
-       "id":1,
-       "bot":"Hot Bot",
-       "description":"Hot bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
-       "index-value":289.34,
-       "cagr":34
+      "id": 1,
+      "bot": "Hot Bot",
+      "description": "Hot bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      "index-value": 289.34,
+      "cagr": 34
     },
     {
-       "id":2,
-       "bot":"Cool Bot",
-       "description":"Cool bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
-       "index-value":439.34,
-       "cagr":28
+      "id": 2,
+      "bot": "Cool Bot",
+      "description": "Cool bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      "index-value": 439.34,
+      "cagr": 28
     },
     {
-       "id":3,
-       "bot":"Options Bot",
-       "description":"Options bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
-       "index-value":139.34,
-       "cagr":42
+      "id": 3,
+      "bot": "Options Bot",
+      "description": "Options bot is low volatility portfolio of growth stocks selected based on our proprietary metrics",
+      "index-value": 139.34,
+      "cagr": 42
     }
- ]
+  ];
 
   return (
+
     <Router>
       <Switch>
         <Route path="/bots">
-          <Bots/>
+          {data.map((eachbot)=>(
+            <Bots botid={eachbot.id} botbot={eachbot.bot} botdisc={eachbot.description} botindex={eachbot['index-value']} botcagr={eachbot.cagr}/>
+          ))}
+          <Bots />
         </Route>
         <Route path="//bots-details/:id=">
-          < Viewalgo/>
+          < Viewalgo />
         </Route>
-        
+
       </Switch>
     </Router >
   );
--- src/components/Bots.js
+++ src/components/Bots.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Bots.css';
+
 import { Link } from 'react-router-dom';
 
 export default function bots(props) {
     return (
-        <div>
-         
-            <h1>hello bots</h1>
-            <link to="/Viewalgo">scs</link>
-        </div>
+        <>
+            <div className="f-container">
+
+                <div className="f-item">{props.botbot}</div>
+                <div className="f-item"><div>Index value</div><div>{props.botindex}</div></div>
+                <div className="f-item" ><div>Cagr</div><div className="cagr">{props.botcagr}</div></div>
+                <div className="f-item"><div>botton1</div><div>button2</div></div>
+
+                {/* <link to="/Viewalgo">scs</link> */}
+            </div>
+            <hr />
+        </>
     )
 }
+
